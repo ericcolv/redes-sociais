edb686b42426017438431216d92bf4e6fd884183
ericcolv <141738866+ericcolv@users.noreply.github.com>
2024-09-26 09:39:04 -0300

    Update informaçõesGlobais.js

--- informaçõesGlobais.js
+++ informaçõesGlobais.js
@@ -1,10 +1,9 @@
-{
-  "total_pessoas_conectadas": 5.04e9,
-  "tempo_medio": 2.38,
-  "total_pessoas_mundo": 7.888e9
-}const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
 async function vizualizarInformacoesGlobais() {
-
+    const res = await fetch(url)
+    const dados = await res.json()
+    console.log(dados);
 }
+
+vizualizarInformacoesGlobais()
